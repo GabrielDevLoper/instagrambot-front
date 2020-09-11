5a4584ea321d0fc5861a783964fc23a9bce043c9
Gabriel Barreto <gabriel.barret015@hotmail.com>
2020-09-11 00:20:18 -0300

    install lib material-ui and created components

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Button } from "@material-ui/core";
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <Button color="secondary" variant="contained">
+        Entrar
+      </Button>
+    </div>
+  );
+};
+
+export default Home;
